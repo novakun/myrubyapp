22806bf0975151efaa2905651126648de13dfc4a
novakun <novakungames@gmail.com>
2017-05-23 11:19:54 +0200

    Added dynamic products
    - Display products within the product page in a clean format

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170522171203) do
+ActiveRecord::Schema.define(version: 20170523081841) do
 
   create_table "products", force: :cascade do |t|
     t.string "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20170522171203) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "cost"
+    t.string "color"
   end
 
 end
