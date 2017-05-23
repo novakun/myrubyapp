30a1c9bb4608508810adfd2c64b33a2d4bfe2160
“novakun” <novakungames@gmail.com>
2017-05-23 08:23:59 +0200

    added cost

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170522083151) do
+ActiveRecord::Schema.define(version: 20170522171203) do
 
   create_table "products", force: :cascade do |t|
     t.string "name"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 20170522083151) do
     t.string "image_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "cost"
   end
 
 end
