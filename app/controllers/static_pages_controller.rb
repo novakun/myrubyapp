b892cc3033f189ff5b92b33e65be943377c9f2fe
novakun <novakungames@gmail.com>
2017-05-31 10:58:35 +0200

    Broken version when trying to do carousel

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,12 @@
 class StaticPagesController < ApplicationController
- 
+  def index
+  	
+  end
+
+
   def landing_page
     @featured_product = Product.first
+    @products = Product.limit(4)
   end
 
 end
